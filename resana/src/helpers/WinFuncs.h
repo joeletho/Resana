14905b810c6219d91ddd6879c47e014f14d26a80
Joel <brolgedu@gmail.com>
2022-05-19 14:46:50 -0700

    Added Win32 helper to print Win32 error messages

--- resana/src/helpers/WinFuncs.h
+++ resana/src/helpers/WinFuncs.h
@@ -1,8 +1,40 @@
-//
-// Created by Joel on 5/13/2022.
-//
+#pragma once
 
-#ifndef RESANA_PRINTERRORMSG_H
-#define RESANA_PRINTERRORMSG_H
+#include <Windows.h>
+#include <strsafe.h>
 
-#endif //RESANA_PRINTERRORMSG_H
+namespace RESANA {
+
+    void PrintErrorMsg(LPTSTR lpszFunction) {
+        // Retrieve the system error message for the last-error code
+        LPVOID lpMsgBuf;
+        LPVOID lpDisplayBuf;
+        DWORD dw = GetLastError();
+
+        FormatMessage(
+                FORMAT_MESSAGE_ALLOCATE_BUFFER |
+                FORMAT_MESSAGE_FROM_SYSTEM |
+                FORMAT_MESSAGE_IGNORE_INSERTS,
+                nullptr,
+                dw,
+                MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                (LPTSTR) &lpMsgBuf,
+                0, nullptr);
+
+        // Display the error message and exit the process
+        lpDisplayBuf = (LPVOID) LocalAlloc(LMEM_ZEROINIT,
+                                           (lstrlen((LPCTSTR) lpMsgBuf) +
+                                            strlen((LPCTSTR) lpszFunction) + 40) *
+                                           sizeof(TCHAR));
+        StringCchPrintf((LPTSTR) lpDisplayBuf,
+                        LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+                        TEXT("%s failed with error %d: %s"),
+                        lpszFunction, dw, lpMsgBuf);
+        RS_CORE_WARN((LPCTSTR) lpDisplayBuf);
+        // MessageBox(nullptr, (LPCTSTR) lpDisplayBuf, TEXT("Error"), MB_OK);
+
+        LocalFree(lpMsgBuf);
+        LocalFree(lpDisplayBuf);
+    }
+
+}
