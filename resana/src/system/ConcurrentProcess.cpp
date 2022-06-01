0ab7ca7a7e1fed940531341afa36e89c22e71d3e
Joel <brolgedu@gmail.com>
2022-05-31 21:58:52 -0700

    Added new base class to handle classes that need built-in concurrency, such as CPU* and Process* classes.

---
+++ resana/src/system/ConcurrentProcess.cpp
@@ -0,0 +1,34 @@
+#include "ConcurrentProcess.h"
+
+#include "helpers/Time.h"
+
+namespace RESANA {
+
+	ConcurrentProcess::ConcurrentProcess(std::string name)
+		: mDebugName(std::move(name)) {}
+
+	void ConcurrentProcess::NotifyOne()
+	{
+		mProcCondition.notify_one();
+	}
+
+	void ConcurrentProcess::NotifyAll()
+	{
+		mProcCondition.notify_all();
+	}
+
+	void ConcurrentProcess::Wait(std::unique_lock<std::mutex>& lock, bool condition)
+	{
+		mProcCondition.wait(lock, [condition]() { return condition; });
+	}
+
+	void ConcurrentProcess::WaitFor(std::unique_lock<std::mutex>& lock, unsigned int milliseconds, bool condition)
+	{
+		while (!condition)
+		{
+			Sleep(milliseconds);
+			mProcCondition.wait(lock, [condition]() { return condition; });
+		}
+	}
+
+}
---
+++ resana/src/system/ConcurrentProcess.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <mutex>
+#include <condition_variable>
+
+namespace RESANA {
+
+	class ConcurrentProcess {
+	public:
+		ConcurrentProcess(std::string name);
+		virtual ~ConcurrentProcess() {};
+
+		virtual void Start() {};
+		virtual void Stop() {};
+
+	protected:
+		void NotifyOne();
+		void NotifyAll();
+		void Wait(std::unique_lock<std::mutex>& lock, bool condition);
+		void WaitFor(std::unique_lock<std::mutex>& lock, unsigned int milliseconds, bool condition);
+
+		inline std::condition_variable& GetCV() { return mProcCondition; };
+
+	private:
+		std::string mDebugName;
+		std::condition_variable mProcCondition;
+	};
+
+}
