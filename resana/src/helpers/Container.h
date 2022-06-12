24870bb7850523eeba72e34403f0e48a51a065d5
Joel <brolgedu@gmail.com>
2022-06-12 09:59:36 -0700

    Added additional helpers for vector class

--- resana/src/helpers/Container.h
+++ resana/src/helpers/Container.h
@@ -7,14 +7,18 @@
 namespace RESANA {
 
 	template<typename T>
-	void Clear(std::vector<T>& vec) {
+	void Clear(std::vector<T>& vec)
+	{
 		vec.erase(vec.begin(), vec.end());
 	}
 
 	template<typename T>
-	void Free(std::vector<T>& vec) {
-		for (T ptr : vec) {
-			if (ptr != nullptr) {
+	void Free(std::vector<T>& vec)
+	{
+		for (T ptr : vec)
+		{
+			if (ptr != nullptr)
+			{
 				ptr = nullptr;
 				free(ptr);
 			}
@@ -22,29 +26,30 @@ namespace RESANA {
 	}
 
 	template<typename T>
-	void Delete(std::vector<T>& vec) {
-		for (T ptr : vec) {
-			delete ptr;
-		}
+	void Delete(std::vector<T>& vec)
+	{
+		for (T ptr : vec) { delete ptr; }
 	}
 
 	template<typename T>
-	void Copy(std::vector<T>& lhs, const std::vector<T>& rhs) {
+	void Copy(std::vector<T>& lhs, const std::vector<T>& rhs)
+	{
 		Clear(lhs);
 		lhs.reserve(rhs.size());
 		std::unique_copy(rhs.begin(), rhs.end(), std::back_inserter(lhs));
 	}
 
 	template<typename T>
-	void Copy(std::vector<std::initializer_list<T>>& lhs, const std::vector<T>& rhs) {
+	void Copy(std::vector<std::initializer_list<T>>& lhs, const std::vector<T>& rhs)
+	{
 		Clear(lhs);
 		lhs.reserve(rhs.size());
 		std::unique_copy(rhs.begin(), rhs.end(), std::back_inserter(lhs));
 	}
 
 	template<typename T>
-	void CopyProtected(std::vector<T>& lhs, const std::vector<T>& rhs, std::mutex& lhs_mtx, std::mutex& rhs_mtx, bool del_lhs) {
-
+	void CopyProtected(std::vector<T>& lhs, const std::vector<T>& rhs, std::mutex& lhs_mtx, std::mutex& rhs_mtx, bool del_lhs)
+	{
 		static std::mutex funcMutex;
 		{
 			std::lock_guard<std::mutex> lock(funcMutex);
@@ -52,6 +57,7 @@ namespace RESANA {
 				std::lock<std::mutex>(lhs_mtx, rhs_mtx);
 				std::lock_guard<std::mutex> lock1(lhs_mtx, std::adopt_lock);
 				std::lock_guard<std::mutex> lock2(rhs_mtx, std::adopt_lock);
+
 				if (del_lhs) { Delete(lhs); }
 				Copy(lhs, rhs);
 			}
