190dbc0654ada03a27653d337b612abd3f7cf19a
Joel <brolgedu@gmail.com>
2022-06-18 10:15:53 -0700

    Added ImGui::Selectable* helper functions in ImGui:: namespace

---
+++ resana/src/imgui/ImGuiHelpers.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <imgui.h>
+
+namespace ImGui {
+
+	// This function will wrap text w/in a selectable
+	static bool SelectableWrapped(const char* label, bool selected, ImGuiSelectableFlags flags, const float window_width) {
+		ImFont* font = GetFont();
+		ImVec2 size = font->CalcTextSizeA(font->FontSize, GetContentRegionMax().x, window_width, label);
+
+		if (Selectable("##hidden", selected, flags, ImVec2(0.0, size.y))) {
+			return true;
+		}
+
+		const ImVec2 p0 = GetItemRectMin();
+		const ImVec2 p1 = GetItemRectMax();
+		const ImVec2 text_pos = ImVec2(p0.x + 5.0f, p0.y);
+		ImDrawList* draw_list = GetWindowDrawList();
+
+		ImVec4 clip_rect(p0.x, p0.y, p1.x + 5.0f, p1.y); // AddText() takes a ImVec4* here so let's convert.
+		draw_list->AddText(font, font->FontSize, text_pos, IM_COL32_WHITE, label, NULL,
+			window_width, &clip_rect);
+
+		return false;
+	}
+
+	static bool SelectableWrapped(const char* label, bool selected, ImGuiSelectableFlags flags, const float window_width, const char* uniqueId) {
+		ImFont* font = GetFont();
+		ImVec2 size = font->CalcTextSizeA(font->FontSize, GetContentRegionMax().x, window_width, label);
+
+		if (Selectable(uniqueId, selected, flags, ImVec2(0.0, size.y))) {
+			return true;
+		}
+
+		const ImVec2 p0 = GetItemRectMin();
+		const ImVec2 p1 = GetItemRectMax();
+		const ImVec2 text_pos = ImVec2(p0.x + 5.0f, p0.y);
+		ImDrawList* draw_list = GetWindowDrawList();
+
+		ImVec4 clip_rect(p0.x, p0.y, p1.x + 5.0f, p1.y); // AddText() takes a ImVec4* here so let's convert.
+		draw_list->AddText(font, font->FontSize, text_pos, IM_COL32_WHITE, label, NULL,
+			window_width, &clip_rect);
+
+		return false;
+	}
+
+		static bool Selectable(const char* label, bool selected, ImGuiSelectableFlags flags, const float window_width, const char* uniqueId) {
+		ImFont* font = GetFont();
+
+		if (Selectable(uniqueId, selected, flags)) {
+			return true;
+		}
+
+		const ImVec2 p0 = GetItemRectMin();
+		const ImVec2 p1 = GetItemRectMax();
+		const ImVec2 text_pos = ImVec2(p0.x + 5.0f, p0.y);
+		ImDrawList* draw_list = GetWindowDrawList();
+
+		ImVec4 clip_rect(p0.x, p0.y, p1.x + 5.0f, p1.y); // AddText() takes a ImVec4* here so let's convert.
+		draw_list->AddText(font, font->FontSize, text_pos, IM_COL32_WHITE, label, NULL,
+			window_width, &clip_rect);
+
+		return false;
+	}
+}
