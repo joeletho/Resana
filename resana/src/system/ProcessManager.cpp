f9d43627c6bb460e540627f04446f3c950aa0597
Joel <brolgedu@gmail.com>
2022-06-18 10:51:26 -0700

    Added atomic bool mDataPrepared to notify waiting threads. Refactored PrepareData(), GetPreparedData() and separated PrepareData and GetPreparedData functions to different threads for efficiency. Abstracted CleanMap and ResetAllRunningStatus functions. Abstracted ProcessEntry into new class. Abstracted and refactored ProcessArray into class ProcessContainer. Added ProcessMap to more efficiently handle ProcessEntry data updating. Added ProcessContainer class to handle external process data retrieval (i.e. ImGui). Better implemented the efficiency of retrieving and processing the data by utilizing threads and thread locks to work together and wake when needed. Further improvements were made to memory and thread management. Removed unnecessary varibles, functions, and includes.

--- resana/src/system/ProcessManager.cpp
+++ resana/src/system/ProcessManager.cpp
@@ -6,48 +6,36 @@
 #include "helpers/FileUtils.h"
 #include "helpers/WinFuncs.h"
 
+#include <Windows.h>
 #include <ProcessSnapshot.h>
 
 namespace RESANA {
 
 	ProcessManager* ProcessManager::sInstance = nullptr;
 
-	ProcessManager::ProcessManager() : ConcurrentProcess("ProcessManager")
+	ProcessManager::ProcessManager()
+		: ConcurrentProcess("ProcessManager"), mDataReady(false), mDataBusy(false)
 	{
-		mProcessEntries.reset(new ProcessArray);
+		mProcessContainer.reset(new ProcessContainer);
 	}
 
 	ProcessManager::~ProcessManager()
 	{
-		Sleep(1000); // Let detached threads finish before destructing
+		Sleep(500); // Let detached threads finish before 
 
 		std::mutex mutex;
 		std::unique_lock<std::mutex> lock(mutex);
 
 		auto& lc = GetLockContainer();
-		auto& writeLock = lc.GetWriteLock();
-		auto& readLock = lc.GetReadLock();
+		while (mDataBusy) { lc.Wait(lock); }
 
-		while (writeLock.owns_lock()) { lc.Wait(lock); }
-
-		writeLock.lock();
-		lc.Wait(writeLock, !readLock.owns_lock());
-
-		mProcessEntries->Destroy();
-
-		while (!mProcessQueue.empty())
-		{
-			auto p = mProcessQueue.front();
-			mProcessQueue.pop();
-
-			sInstance = nullptr;
-		}
+		sInstance = nullptr;
 	}
 
 	void ProcessManager::Terminate()
 	{
 		mRunning = false;
-		this->~ProcessManager();
+		delete this;
 	}
 
 	ProcessManager* ProcessManager::Get()
@@ -59,34 +47,48 @@ namespace RESANA {
 		return sInstance;
 	}
 
-	std::shared_ptr<ProcessArray> ProcessManager::GetData()
+	std::shared_ptr<ProcessContainer> ProcessManager::GetData()
 	{
 		RS_CORE_ASSERT(mRunning, "Process is not currently running! Call 'ProcessManager::Run()' to start process.");
 
-		if (!mDataReady) { return {}; }
-
+		std::mutex mutex;
+		std::unique_lock<std::mutex> lock(mutex);
 		auto& lc = GetLockContainer();
-		auto& readLock = lc.GetReadLock();
-		auto& writeLock = lc.GetWriteLock();
 
-		readLock.lock();
-		lc.Wait(readLock, !writeLock.owns_lock()); // Don't read the data when it's being set!
+		if (!mDataReady)
+		{
+			// We wait a little bit to make the UI more streamlined -- otherwise, we may
+			// see some random flickering.
+			lc.WaitFor(lock, std::chrono::milliseconds(10), mDataReady);
+		}
+		if (!mDataReady) { return {}; } // Is data is still not ready, return nothing.
+
+		mDataBusy = true;
 		lc.NotifyAll();
 
-		return mProcessEntries;
+		return mProcessContainer;
 	}
 
 	void ProcessManager::ReleaseData()
 	{
+		mDataBusy = false;
 		auto& lc = GetLockContainer();
-		auto& readLock = lc.GetReadLock();
-
-		readLock.unlock();
 		lc.NotifyAll();
 	}
-	int ProcessManager::GetNumEntries() const
+
+	void ProcessManager::ResetAllRunningStatus()
 	{
-		return (mProcessEntries) ? mProcessEntries->Entries.size() : 0;
+		// Set all process running status to false
+		std::scoped_lock slock(mProcessMap.GetMutex());
+		for (const auto& [id, entry] : mProcessMap)
+		{
+			entry->Running() = false;
+		}
+	}
+
+	int ProcessManager::GetNumProcesses() const
+	{
+		return (mProcessContainer) ? mProcessContainer->GetNumEntries() : 0;
 	}
 
 	void ProcessManager::Run()
@@ -95,8 +97,8 @@ namespace RESANA {
 
 		if (!sInstance->mRunning)
 		{
-			sInstance->mThreads.push_back(std::thread([&] { sInstance->ProcessAndSetDataThread(); }));
-			sInstance->mThreads.back().detach();
+			auto prepThread = std::thread([&] { sInstance->PrepareDataThread(); }); prepThread.detach();
+			auto processThread = std::thread([&] { sInstance->ProcessDataThread(); }); processThread.detach();
 			sInstance->mRunning = true;
 		}
 	}
@@ -106,45 +108,78 @@ namespace RESANA {
 		if (sInstance && sInstance->mRunning)
 		{
 			sInstance->mRunning = false;
-			std::thread kill([&]() { sInstance->Terminate(); });
-			kill.detach();
+			std::thread kill([&]() { sInstance->Terminate(); }); kill.detach();
 		}
 	}
 
-	void ProcessManager::ProcessAndSetDataThread()
+	void ProcessManager::PrepareDataThread()
 	{
+		auto& lc = GetLockContainer();
+
 		while (mRunning)
 		{
-			auto data = GetProcessData();
-			SetData(data);
-			Sleep(1000);
+			if (PrepareData())
+			{
+				// Notify waiting threads	
+				mDataPrepared = true;
+				lc.NotifyAll();
+			}
+			Time::Sleep(1000);
 		}
 	}
 
-	ProcessArray* ProcessManager::GetProcessData()
+	void ProcessManager::ProcessDataThread()
 	{
-		HANDLE hProcessSnap;
-		ProcessArray* data = nullptr;
-		static PROCESSENTRY32 processEntry{};
+		std::vector<std::thread> localThreads;
+
+		while (mRunning)
+		{
+			auto data = GetPreparedData();
+			localThreads.emplace_back(std::thread([&data, this]() {SetData(std::ref(data)); }));
+		}
+
+		// Join any remaining threads before terminating
+		for (auto& th : localThreads)
+		{
+			if (th.joinable()) { th.join(); }
+		}
+	}
+
+	bool ProcessManager::PrepareData()
+	{
+		PROCESSENTRY32 processEntry{};
 		processEntry.dwSize = sizeof(PROCESSENTRY32);
 
 		// Take a snapshot of all processes in the system.
-		hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+		HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 		if (hProcessSnap == INVALID_HANDLE_VALUE) {
 			PrintWin32Error("CreateToolhelp32Snapshot (of processes)");
+			return false;
 		}
 
 		if (!Process32First(hProcessSnap, &processEntry)) {
 			PrintWin32Error("Process32First");
 			CloseHandle(hProcessSnap);          // clean the snapshot object
+			return false;
 		}
 
-		data = new ProcessArray();
+		ResetAllRunningStatus();
 
 		// Now walk the snapshot of processes, and
 		// get information about each process in turn
-		do {
-			data->Entries.emplace_back(new ProcessEntry(processEntry));
+		do
+		{
+			{
+				std::scoped_lock slock(mProcessMap.GetMutex());
+
+				// Set process running status to true and
+				//	update process, if applicable
+				if (!UpdateProcess(processEntry))
+				{
+					// Otherwise, add new process
+					mProcessMap.Emplace(new ProcessEntry(processEntry));
+				}
+			}
 
 			/* TODO: Implement these features
 			// List the modules and threads associated with this process
@@ -156,34 +191,126 @@ namespace RESANA {
 
 		CloseHandle(hProcessSnap);
 
+		// Remove any processes not currently running
+		CleanMap();
+
+		return true;
+	}
+
+	ProcessContainer* ProcessManager::GetPreparedData()
+	{
+		std::mutex mutex;
+		std::unique_lock<std::mutex> lock(mutex);
+
+		auto& lc = GetLockContainer();
+		while (!mDataPrepared) { lc.Wait(lock); }
+
+		ProcessContainer* data = nullptr;
+		{
+			std::scoped_lock slock(mProcessMap.GetMutex());
+
+			// Add a deep copy to the ProcessContainer to preserve mProcessMap data
+			data = new ProcessContainer();
+			for (auto& [id, entry] : mProcessMap)
+			{
+				auto* copy = new ProcessEntry(entry);
+				data->AddEntry(copy);
+			}
+		}
+		// Unset the flag
+		mDataPrepared = false;
+
 		return data;
 	}
 
-	void ProcessManager::SetData(ProcessArray* data)
+	void ProcessManager::SetData(ProcessContainer* data)
 	{
-		if (!data) { return; }
+		if (!sInstance || !data) { return; }
+
+		// Update selected entry before submitting.
+		auto syncThread = std::thread([&data, this]() { SyncSelectionStatus(std::ref(data)); });
 
 		std::mutex mutex;
 		std::unique_lock<std::mutex> lock(mutex);
 
 		auto& lc = GetLockContainer();
-		auto& writeLock = lc.GetWriteLock();
-		auto& readLock = lc.GetReadLock();
-
-		while (writeLock.owns_lock()) { lc.Wait(lock); }
-
-		writeLock.lock();
-		lc.Wait(writeLock, !readLock.owns_lock()); // Don't set the data while it's being read!
+		while (mDataBusy) { lc.Wait(lock); }
 
 		mDataReady = false;
 		lc.NotifyAll(); // Notify all that the state has changed
 
-		mProcessEntries->Destroy();
-		mProcessEntries.reset(data);
-		mDataReady = true;
+		syncThread.join();
 
-		writeLock.unlock();
+		// Reset process entries
+		mProcessContainer.reset(data);
+
+		mDataReady = true;
 		lc.NotifyAll();
+	}
+
+	bool ProcessManager::UpdateProcess(const ProcessEntry* entry) const
+	{
+		if (!entry) { return false; }
+
+		if (const auto& proc = mProcessMap.Find(entry->ProcessId()))
+		{
+			std::scoped_lock slock(proc->Mutex());
+
+			if (proc != entry) { // Don't update if there's no change
+				proc->operator=(entry);
+			}
+
+			proc->Running() = true;
+
+			return true;
+		}
+		return false;
+	}
+
+	bool ProcessManager::UpdateProcess(const PROCESSENTRY32& pe32) const
+	{
+		if (const auto& proc = mProcessMap.Find(pe32.th32ProcessID))
+		{
+			std::scoped_lock slock(proc->Mutex());
+
+			if (proc->operator!=(pe32)) { // Don't update if there's no change
+				proc->operator=(pe32);
+			}
+
+			proc->Running() = true;
+
+			return true;
+		}
+		return false;
+	}
+
+	void ProcessManager::SyncSelectionStatus(ProcessContainer* data) const
+	{
+		if (const auto& selectedEntry = mProcessContainer->GetSelectedEntry())
+		{
+			const auto& entries = data->GetEntries();
+			for (auto& entry : entries)
+			{
+				if (entry->ProcessId() == selectedEntry->ProcessId()) {
+					data->SelectEntry(entry);
+					break;
+				}
+			}
+		}
+	}
+
+	void ProcessManager::CleanMap()
+	{
+		std::scoped_lock slock(mProcessMap.GetMutex());
+
+		// Remove any processes not currently running
+		for (auto it = mProcessMap.begin(); it != mProcessMap.end(); ++it)
+		{
+			if (!it->second->Running()) {
+				mProcessMap.Erase(it->second);
+				it = mProcessMap.begin(); // Reset iterator!
+			}
+		}
 	}
 
 }
