c2a72782ee63a1abd1e8e1a1a4af5d89de7051d4
Joel <brolgedu@gmail.com>
2022-06-24 11:13:13 -0700

    Timestep class: Added operator = and > overloads to class. TimeTick class: Added new TimeTick class to operate a clock tick that can be used as a synchronizing event when needed. TimeTick utilizes its own thread to keep a running tick that, once started, stops only when notified. This will most likely be depreciated once a proper event system has been implemented.

--- resana/src/helpers/Time.h
+++ resana/src/helpers/Time.h
@@ -1,85 +1,109 @@
 #pragma once
 
-#include "rspch.h"
-
 namespace RESANA {
 
 #define CLOCKS_PER_HOUR 3600000;
 #define CLOCKS_PER_MIN 60000;
 
-    //--------------------------------------------------------------
-    // [SECTION] Time
-    //--------------------------------------------------------------
+	//--------------------------------------------------------------
+	// [SECTION] Time
+	//--------------------------------------------------------------
 
-    class Time {
-    public:
-        static float GetTimeSeconds();
-        static float GetTimeMilliseconds();
-        static float GetTimeNanoseconds();
+	class Time {
+	public:
+		static float GetTimeSeconds();
+		static float GetTimeMilliseconds();
+		static float GetTimeNanoseconds();
 
-        static long long GetTime();
-        static std::string GetTimeFormatted();
-        static void Sleep(int sleep_time_ms);
+		static long long GetTime();
+		static std::string GetTimeFormatted();
+		static void Sleep(int sleep_time_ms);
 
-    private:
-        Time();
-    private:
-        static std::chrono::steady_clock::time_point mTimeStarted;
+	private:
+		Time();
+	private:
+		static std::chrono::steady_clock::time_point mTimeStarted;
 
-        [[maybe_unused]] static Time *sInstance;
-    };
+		[[maybe_unused]] static Time* sInstance;
+	};
 
-    class Timestep {
-    public:
-        explicit Timestep(float time = 0.0f) : mTime(time) {}
+	class Timestep {
+	public:
+		Timestep(float time = 0.0f) : mTime(time) {}
 
-        operator float() const { return mTime; }
+		operator float() const { return mTime; }
 
-        [[nodiscard]] float GetSeconds() const { return mTime; }
+		Timestep& operator=(const int ms)
+		{
+			mTime = ms / 1000.0f;
+			return *this;
+		}
 
-        [[nodiscard]] float GetMilliseconds() const { return mTime * 1000.0f; }
+		[[nodiscard]] float GetSeconds() const { return mTime; }
 
-    private:
-        float mTime;
-    };
+		[[nodiscard]] float GetMilliseconds() const { return mTime * 1000.0f; }
 
-    //--------------------------------------------------------------
-    // [SECTION] Stop Watch
-    //--------------------------------------------------------------
+		bool operator>(const Timestep rhs) const
+		{
+			return mTime > rhs;
+		};
 
-    class StopWatch {
-    public:
-        StopWatch();
+	private:
+		float mTime;
+	};
 
-        void Start();
-        void Stop();
-        [[nodiscard]] float GetSeconds() const;
-        [[nodiscard]] unsigned int GetHours() const;
-        [[nodiscard]] unsigned int GetMinutes() const;
-        [[nodiscard]] unsigned int GetMilliseconds() const;
-        std::string GetElapsedTime();
+	class TimeTick {
+	public:
+		TimeTick();
+		~TimeTick();
+		void Stop();
+
+		Timestep GetTickCount() const { return mTick; }
+
+	private:
+		void CountTicks();
+
+		Timestep mTick = 0;
+		bool mRunning = false;
+	};
+
+	//--------------------------------------------------------------
+	// [SECTION] Stop Watch
+	//--------------------------------------------------------------
+
+	class StopWatch {
+	public:
+		StopWatch();
+
+		void Start();
+		void Stop();
+		[[nodiscard]] float GetSeconds() const;
+		[[nodiscard]] unsigned int GetHours() const;
+		[[nodiscard]] unsigned int GetMinutes() const;
+		[[nodiscard]] unsigned int GetMilliseconds() const;
+		std::string GetElapsedTime();
 
 
-    private:
-        void CalculateTime();
-        void CalculateElapsedTime();
+	private:
+		void CalculateTime();
+		void CalculateElapsedTime();
 
-    private:
-        std::string sElapsedTime;
-        std::chrono::steady_clock::time_point sStartTime;
-        std::chrono::steady_clock::time_point sStopTime;
-        bool mStarted = false;
+	private:
+		std::string sElapsedTime;
+		std::chrono::steady_clock::time_point sStartTime;
+		std::chrono::steady_clock::time_point sStopTime;
+		bool mStarted = false;
 
-    private:
-        struct TimeData {
-            unsigned int Hours;
-            unsigned int Minutes;
-            unsigned long Milliseconds;
-            unsigned long Nanoseconds;
-            float Seconds;
-        };
-        TimeData sData{};
-    };
+	private:
+		struct TimeData {
+			unsigned int Hours;
+			unsigned int Minutes;
+			unsigned long Milliseconds;
+			unsigned long Nanoseconds;
+			float Seconds;
+		};
+		TimeData sData{};
+	};
 
 }
 
