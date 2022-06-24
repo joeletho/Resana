9f51887a350bcda7f6fd547dbcc40e8c4fa5620d
Joel <brolgedu@gmail.com>
2022-06-24 11:28:58 -0700

    Expanded upon Panel, which now inherits from Layer class. Added RefreshRate enum for now and will likely change in the future.

--- resana/src/panels/Panel.h
+++ resana/src/panels/Panel.h
@@ -1,12 +1,26 @@
 #pragma once
 
+#include "core/Layer.h"
+
+#include "helpers/Time.h"
+
 namespace RESANA {
 
-    class Panel {
-    public:
-        virtual ~Panel() = default;
+		enum RefreshRate {
+		Fast = 500,
+		Normal = 1000,
+		Slow = 2000
+	};
 
-        virtual void ShowPanel(bool* pOpen) = 0;
-    };
+	class Panel : public Layer {
+	public:
+		~Panel() override = default;
+		void OnAttach() override = 0;
+		void OnDetach() override = 0;
+		void OnUpdate(Timestep ts) override = 0;
+		void OnImGuiRender() override = 0;
+		virtual void ShowPanel(bool* pOpen) = 0;
+		virtual void SetTickRate(Timestep tickRate) {};
+	};
 
 }
