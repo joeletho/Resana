344b3804cb608d956449c27ccee8839963ee762b
Joel <brolgedu@gmail.com>
2022-06-27 10:49:00 -0700

    Formatting.

--- resana/src/core/Log.h
+++ resana/src/core/Log.h
@@ -9,7 +9,7 @@ namespace RESANA {
     public:
         static void Init();
 
-        inline static std::shared_ptr<spdlog::logger> &GetCoreLogger() { return sCoreLogger; }
+        static std::shared_ptr<spdlog::logger> &GetCoreLogger() { return sCoreLogger; }
 
     private:
         static std::shared_ptr<spdlog::logger> sCoreLogger;
@@ -18,8 +18,8 @@ namespace RESANA {
 } // RESANA
 
 // Core log macros
-#define RS_CORE_TRACE(...)      ::RESANA::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define RS_CORE_INFO(...)       ::RESANA::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define RS_CORE_WARN(...)       ::RESANA::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define RS_CORE_ERROR(...)      ::RESANA::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define RS_CORE_CRITICAL(...)      ::RESANA::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define RS_CORE_TRACE(...)          ::RESANA::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define RS_CORE_INFO(...)           ::RESANA::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define RS_CORE_WARN(...)           ::RESANA::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define RS_CORE_ERROR(...)          ::RESANA::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define RS_CORE_CRITICAL(...)       ::RESANA::Log::GetCoreLogger()->critical(__VA_ARGS__)
