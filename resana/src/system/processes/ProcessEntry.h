67922fc84555acc696b8d4840fab2d7dab67b93e
Joel <brolgedu@gmail.com>
2022-07-02 15:50:56 -0700

    Maintainence

--- resana/src/system/processes/ProcessEntry.h
+++ resana/src/system/processes/ProcessEntry.h
@@ -7,150 +7,147 @@
 
 #include <TlHelp32.h>
 
-namespace RESANA
-{
+namespace RESANA {
 
-	class ProcessEntry
-	{
-		typedef unsigned long ulong;
+class ProcessEntry {
+    typedef unsigned long ulong;
 
-		struct Process
-		{
-			std::string			Name{};
-			ulong				ProcessId{};
-			ulong				ParentProcessId{};
-			ulong				ModuleId{};
-			ulong				MemoryUsage{};
-			ulong				ThreadCount{};
-			ulong				PriorityClass{};
-			ulong				Flags{};
+    struct Process {
+        std::string Name {};
+        ulong ProcessId {};
+        ulong ParentProcessId {};
+        ulong ModuleId {};
+        ulong MemoryUsage {};
+        ulong ThreadCount {};
+        ulong PriorityClass {};
+        ulong Flags {};
 
-			explicit Process(const PROCESSENTRY32& pe32)
-			{
-				Name.assign(pe32.szExeFile);
-				ProcessId				= pe32.th32ProcessID;
-				ParentProcessId			= pe32.th32ParentProcessID;
-				ModuleId				= pe32.th32ModuleID;
-				MemoryUsage				= pe32.cntUsage;
-				ThreadCount				= pe32.cntThreads;
-				PriorityClass			= pe32.pcPriClassBase;
-				Flags					= pe32.dwFlags;
-			}
+        explicit Process(const PROCESSENTRY32& pe32)
+        {
+            Name.assign(pe32.szExeFile);
+            ProcessId = pe32.th32ProcessID;
+            ParentProcessId = pe32.th32ParentProcessID;
+            ModuleId = pe32.th32ModuleID;
+            MemoryUsage = pe32.cntUsage;
+            ThreadCount = pe32.cntThreads;
+            PriorityClass = pe32.pcPriClassBase;
+            Flags = pe32.dwFlags;
+        }
 
-			explicit Process(const ProcessEntry* other)
-			{
-				Name.assign(other->GetName());
-				ProcessId				= other->GetProcessId();
-				ParentProcessId			= other->GetParentProcessId();
-				ModuleId				= other->GetModuleId();
-				MemoryUsage				= other->GetMemoryUsage();
-				ThreadCount				= other->GetThreadCount();
-				PriorityClass			= other->GetPriorityClass();
-				Flags					= other->GetFlags();
-			}
-		};
+        explicit Process(const ProcessEntry* other)
+        {
+            Name.assign(other->GetName());
+            ProcessId = other->GetProcessId();
+            ParentProcessId = other->GetParentProcessId();
+            ModuleId = other->GetModuleId();
+            MemoryUsage = other->GetMemoryUsage();
+            ThreadCount = other->GetThreadCount();
+            PriorityClass = other->GetPriorityClass();
+            Flags = other->GetFlags();
+        }
+    };
 
-	public:
-		explicit ProcessEntry(const PROCESSENTRY32& pe32)
-			: mProcess(pe32), mLock(mMutex, std::defer_lock)
-		{
-			this->operator=(pe32);
-		}
+public:
+    explicit ProcessEntry(const PROCESSENTRY32& pe32)
+        : mProcess(pe32)
+        , mLock(mMutex, std::defer_lock)
+    {
+        this->operator=(pe32);
+    }
 
-		explicit ProcessEntry(const ProcessEntry* entry)
-			: mProcess(entry), mLock(mMutex, std::defer_lock)
-		{
-			this->operator=(entry);
-		}
+    explicit ProcessEntry(const ProcessEntry* entry)
+        : mProcess(entry)
+        , mLock(mMutex, std::defer_lock)
+    {
+        this->operator=(entry);
+    }
 
-		~ProcessEntry()
-		{
-			// Acquire mutex and release upon destruction.
-			std::scoped_lock lock(mMutex);
-		}
+    ~ProcessEntry()
+    {
+        // Acquire mutex and release upon destruction.
+        std::scoped_lock lock(mMutex);
+    }
 
-		[[nodiscard]] ulong GetMemoryUsage() const		{ return mProcess.MemoryUsage; }
-		[[nodiscard]] ulong GetProcessId() const		{ return mProcess.ProcessId; }
-		[[nodiscard]] ulong GetModuleId() const		{ return mProcess.ModuleId; }
-		[[nodiscard]] ulong GetThreadCount() const		{ return mProcess.ThreadCount; }
-		[[nodiscard]] ulong GetParentProcessId() const { return mProcess.ParentProcessId; }
-		[[nodiscard]] ulong GetFlags() const			{ return mProcess.Flags; }
-		[[nodiscard]] std::string GetName() const		{ return mProcess.Name; }
-		[[nodiscard]] ulong GetPriorityClass() const	{ return mProcess.PriorityClass; }
+    [[nodiscard]] ulong GetMemoryUsage() const { return mProcess.MemoryUsage; }
+    [[nodiscard]] ulong GetProcessId() const { return mProcess.ProcessId; }
+    [[nodiscard]] ulong GetModuleId() const { return mProcess.ModuleId; }
+    [[nodiscard]] ulong GetThreadCount() const { return mProcess.ThreadCount; }
+    [[nodiscard]] ulong GetParentProcessId() const { return mProcess.ParentProcessId; }
+    [[nodiscard]] ulong GetFlags() const { return mProcess.Flags; }
+    [[nodiscard]] std::string GetName() const { return mProcess.Name; }
+    [[nodiscard]] ulong GetPriorityClass() const { return mProcess.PriorityClass; }
 
-		void Free() { this->~ProcessEntry(); }
+    void Free() { this->~ProcessEntry(); }
 
-		[[nodiscard]] bool IsSelected() const		{ return mSelected; }
-		bool& Running()								{ return mRunning; }
-		std::mutex& Mutex()							{ return mMutex; }
+    [[nodiscard]] bool IsSelected() const { return mSelected; }
+    bool& Running() { return mRunning; }
+    std::mutex& Mutex() { return mMutex; }
 
+    // Overloads
+    ProcessEntry& operator=(const ProcessEntry* entry)
+    {
+        mProcess.Name.assign(entry->GetName());
+        mProcess.ProcessId = entry->GetProcessId();
+        mProcess.ParentProcessId = entry->GetParentProcessId();
+        mProcess.ModuleId = entry->GetModuleId();
+        mProcess.MemoryUsage = entry->GetMemoryUsage();
+        mProcess.ThreadCount = entry->GetThreadCount();
+        mProcess.PriorityClass = entry->GetPriorityClass();
+        mProcess.Flags = entry->GetFlags();
+        mSelected = entry->IsSelected();
+        return *this;
+    }
 
-		// Overloads
-		ProcessEntry& operator=(const ProcessEntry* entry)
-		{
-			mProcess.Name.assign(entry->GetName());
-			mProcess.ProcessId			= entry->GetProcessId();
-			mProcess.ParentProcessId	= entry->GetParentProcessId();
-			mProcess.ModuleId			= entry->GetModuleId();
-			mProcess.MemoryUsage		= entry->GetMemoryUsage();
-			mProcess.ThreadCount		= entry->GetThreadCount();
-			mProcess.PriorityClass		= entry->GetPriorityClass();
-			mProcess.Flags				= entry->GetFlags();
-			mSelected					= entry->IsSelected();
-			return *this;
-		}
+    ProcessEntry& operator=(const PROCESSENTRY32& pe32)
+    {
+        mProcess.Name.assign(pe32.szExeFile);
+        mProcess.ProcessId = pe32.th32ProcessID;
+        mProcess.ParentProcessId = pe32.th32ParentProcessID;
+        mProcess.ModuleId = pe32.th32ModuleID;
+        mProcess.MemoryUsage = pe32.cntUsage;
+        mProcess.ThreadCount = pe32.cntThreads;
+        mProcess.PriorityClass = pe32.pcPriClassBase;
+        mProcess.Flags = pe32.dwFlags;
+        return *this;
+    }
 
-		ProcessEntry& operator=(const PROCESSENTRY32& pe32)
-		{
-			mProcess.Name.assign(pe32.szExeFile);
-			mProcess.ProcessId			= pe32.th32ProcessID;
-			mProcess.ParentProcessId	= pe32.th32ParentProcessID;
-			mProcess.ModuleId			= pe32.th32ModuleID;
-			mProcess.MemoryUsage		= pe32.cntUsage;
-			mProcess.ThreadCount		= pe32.cntThreads;
-			mProcess.PriorityClass		= pe32.pcPriClassBase;
-			mProcess.Flags				= pe32.dwFlags;
-			return *this;
-		}
+    bool operator==(const PROCESSENTRY32 pe32) const
+    {
+    	return mProcess.Name == pe32.szExeFile ||
+			mProcess.ProcessId == pe32.th32ProcessID ||
+			mProcess.ParentProcessId == pe32.th32ParentProcessID ||
+			mProcess.ModuleId == pe32.th32ModuleID ||
+			mProcess.MemoryUsage == pe32.cntUsage ||
+			mProcess.ThreadCount == pe32.cntThreads ||
+			mProcess.PriorityClass == pe32.pcPriClassBase ||
+			mProcess.Flags == pe32.dwFlags;
+    }
 
-		bool operator==(const PROCESSENTRY32 pe32) const
-		{
-			return mProcess.Name == pe32.szExeFile ||
-				mProcess.ProcessId == pe32.th32ProcessID ||
-				mProcess.ParentProcessId == pe32.th32ParentProcessID ||
-				mProcess.ModuleId == pe32.th32ModuleID ||
-				mProcess.MemoryUsage == pe32.cntUsage ||
-				mProcess.ThreadCount == pe32.cntThreads ||
-				mProcess.PriorityClass == pe32.pcPriClassBase ||
-				mProcess.Flags == pe32.dwFlags;
+    bool operator!=(const PROCESSENTRY32& pe32) const
+    {
+    	return mProcess.Name != pe32.szExeFile ||
+			mProcess.ProcessId != pe32.th32ProcessID ||
+			mProcess.ParentProcessId != pe32.th32ParentProcessID ||
+			mProcess.ModuleId != pe32.th32ModuleID ||
+			mProcess.MemoryUsage != pe32.cntUsage ||
+			mProcess.ThreadCount != pe32.cntThreads ||
+			mProcess.PriorityClass != pe32.pcPriClassBase ||
+			mProcess.Flags != pe32.dwFlags;
+    }
 
-		}
+private:
+    void Select() { mSelected = true; }
+    void Deselect() { mSelected = false; }
 
-		bool operator!=(const PROCESSENTRY32& pe32) const
-		{
-			return mProcess.Name != pe32.szExeFile ||
-				mProcess.ProcessId != pe32.th32ProcessID ||
-				mProcess.ParentProcessId != pe32.th32ParentProcessID ||
-				mProcess.ModuleId != pe32.th32ModuleID ||
-				mProcess.MemoryUsage != pe32.cntUsage ||
-				mProcess.ThreadCount != pe32.cntThreads ||
-				mProcess.PriorityClass != pe32.pcPriClassBase ||
-				mProcess.Flags != pe32.dwFlags;
-		}
+private:
+    Process mProcess;
+    std::mutex mMutex {};
+    std::unique_lock<std::mutex> mLock {};
+    bool mRunning = true;
+    bool mSelected = false;
 
-	private:
-		void Select() { mSelected = true; }
-		void Deselect() { mSelected = false; }
-
-	private:
-		Process mProcess;
-		std::mutex mMutex{};
-		std::unique_lock<std::mutex> mLock{};
-		bool mRunning = true;
-		bool mSelected = false;
-
-		friend class ProcessManager;
-		friend class ProcessContainer;
-	};
+    friend class ProcessManager;
+    friend class ProcessContainer;
+};
 
 }
--- resana/src/system/processes/ProcessManager.cpp
+++ resana/src/system/processes/ProcessManager.cpp
@@ -6,388 +6,388 @@
 #include "helpers/FileUtils.h"
 #include "helpers/WinFuncs.h"
 
-#include <Windows.h>
-#include <TlHelp32.h>
 #include <ProcessSnapshot.h>
+#include <TlHelp32.h>
+#include <Windows.h>
 
 #include "core/Application.h"
 
 namespace RESANA {
 
-	ProcessManager* ProcessManager::sInstance = nullptr;
-
-	ProcessManager::ProcessManager()
-		: ConcurrentProcess("ProcessManager"), mDataReady(false), mDataBusy(false), mUpdateInterval(TimeTick::Rate::Normal)
-	{
-		mProcessContainer.reset(new ProcessContainer);
-	}
-
-	ProcessManager::~ProcessManager()
-	{
-		Time::Sleep(mUpdateInterval); // Let detached threads finish before 
-
-		std::mutex mutex;
-		std::unique_lock<std::mutex> lock(mutex);
-
-		auto& lc = GetLockContainer();
-		while (mDataBusy) { lc.Wait(lock); }
-	}
-
-	void ProcessManager::Destroy() const
-	{
-		sInstance = nullptr;
-		delete this;
-	}
-
-	ProcessManager* ProcessManager::Get()
-	{
-		if (!sInstance) {
-			sInstance = new ProcessManager();
-		}
-
-		return sInstance;
-	}
-
-	std::shared_ptr<ProcessContainer> ProcessManager::GetData()
-	{
-		RS_CORE_ASSERT(IsRunning(), "Process is not currently running! Call 'ProcessManager::Run()' to start process.");
-
-		if (!mDataReady) { return {}; } // Is data is still not ready, return nothing.
-
-		mDataBusy = true;
-
-		return mProcessContainer;
-	}
-
-	void ProcessManager::ReleaseData()
-	{
-		RS_CORE_ASSERT(mDataBusy, "Data is not owned! Did you forget to call 'GetData()'?");
-		mDataBusy = false;
-		auto& lc = GetLockContainer();
-		lc.NotifyAll();
-	}
-
-	void ProcessManager::SetUpdateInterval(Timestep interval)
-	{
-		mUpdateInterval = interval;
-	}
-
-	uint32_t ProcessManager::GetUpdateSpeed() const
-	{
-		return mUpdateInterval;
-	}
-
-	bool ProcessManager::IsRunning() const
-	{
-		return mRunning;
-	}
-
-	int ProcessManager::GetNumProcesses() const
-	{
-		return (mProcessContainer) ? mProcessContainer->GetNumEntries() : 0;
-	}
-
-	void ProcessManager::Run()
-	{
-		if (!sInstance) { sInstance = Get(); }
-
-		if (!sInstance->IsRunning())
-		{
-			sInstance->mRunning = true;
-
-			auto& app = Application::Get();
-			auto& threadPool = app.GetThreadPool();
-
-			threadPool.Queue([&] { sInstance->PrepareDataThread(); });
-			threadPool.Queue([&] { sInstance->ProcessDataThread(); });
-
-		}
-	}
-
-	void ProcessManager::Stop()
-	{
-		if (sInstance && sInstance->IsRunning())
-		{
-			sInstance->mRunning = false;
-			auto& lc = sInstance->GetLockContainer();
-			lc.NotifyAll();
-		}
-	}
-
-	void ProcessManager::Shutdown()
-	{
-		if (sInstance)
-		{
-			Stop();
-
-			auto& app = Application::Get();
-			auto& threadPool = app.GetThreadPool();
-			threadPool.Queue([&]() { sInstance->Destroy(); });
-			sInstance = nullptr;
-		}
-	}
-
-	void ProcessManager::PrepareDataThread()
-	{
-		auto& lc = GetLockContainer();
-
-		while (IsRunning())
-		{
-			if (PrepareData())
-			{
-				// Notify waiting threads
-				mDataPrepared = true;
-				lc.NotifyAll();
-			}
-			Time::Sleep(mUpdateInterval);
-		}
-	}
-
-	void ProcessManager::ProcessDataThread()
-	{
-		while (IsRunning())
-		{
-			auto* data = GetPreparedData();
-			SetData(data);
-		}
-	}
-
-	bool ProcessManager::PrepareData()
-	{
-		HANDLE hProcessSnap{};
-		PROCESSENTRY32 processEntry{};
-		processEntry.dwSize = sizeof(PROCESSENTRY32);
-
-		bool error = false;
-		bool snapshotReady = false;
-		bool processDone = false;
-		bool walkingDone = false;
-
-		auto& app = Application::Get();
-		auto& threadPool = app.GetThreadPool();
-
-		threadPool.Queue([&] {
-			snapshotReady = false;
-			// Take a snapshot of all processes in the system.
-			hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-			if (hProcessSnap == INVALID_HANDLE_VALUE) {
-				PrintWin32Error("CreateToolhelp32Snapshot (of processes)");
-				error = true;
-			}
-			snapshotReady = true;
-			});
-
-		while (!snapshotReady) {
-			Time::Sleep(1);
-			if (error || !IsRunning()) {
-				return false;
-			}
-		}
-
-		threadPool.Queue([&] {
-			processDone = false;
-			if (!Process32First(hProcessSnap, &processEntry))
-			{
-				PrintWin32Error("Process32First");
-				CloseHandle(hProcessSnap); // clean the snapshot object
-				error = true;
-			}
-			processDone = true;
-			});
-
-		ResetAllRunningStatus();
-
-		// Now walk the snapshot of processes, and
-		// get information about each process in turn
-
-		while (!processDone) {
-			Time::Sleep(1);
-			if (error || !IsRunning()) {
-				return false;
-			}
-		}
-
-		threadPool.Queue([&, this] {
-			walkingDone = false;
-			do
-			{
-				{
-					std::lock_guard lock2(mProcessMap.GetMutex());
-
-					// Set process running status to true and
-					//	update process, if applicable
-					if (!UpdateProcess(processEntry))
-					{
-						// Otherwise, add new process
-						mProcessMap.Emplace(new ProcessEntry(processEntry));
-					}
-				}
-
-				/* TODO: Implement these features
-				// List the modules and threads associated with this process
-				ListProcessModules(procEntry.th32ProcessID);
-				ListProcessThreads(procEntry.th32ProcessID);
-				*/
-
-			} while (Process32Next(hProcessSnap, &processEntry));
-
-			walkingDone = true;
-
-			CloseHandle(hProcessSnap);
-			});
-
-		while (!walkingDone) {
-			Time::Sleep(1);
-			if (error || !IsRunning()) {
-				return false;
-			}
-		}
-
-		// Remove any processes not currently running
-		CleanMap();
-
-		return true;
-	}
-
-	ProcessContainer* ProcessManager::GetPreparedData()
-	{
-		std::mutex mutex;
-		std::unique_lock<std::mutex> lock(mutex);
-
-		auto& lc = GetLockContainer();
-		while (!mDataPrepared)
-		{
-			if (!IsRunning()) { return nullptr; }
-			lc.Wait(lock);
-		}
-
-		ProcessContainer* data = nullptr;
-		lock.unlock();
-		std::lock(mutex, mProcessMap.GetMutex());
-		{
-			std::lock_guard lock1(mutex, std::adopt_lock);
-			std::lock_guard lock2(mProcessMap.GetMutex(), std::adopt_lock);
-
-			// Add a deep copy to the ProcessContainer to preserve mProcessMap data
-			data = new ProcessContainer();
-			for (auto& [id, entry] : mProcessMap)
-			{
-				auto* copy = new ProcessEntry(entry);
-				data->AddEntry(copy);
-			}
-		}
-		// Unset the flag
-		mDataPrepared = false;
-		lc.NotifyAll();
-
-		return data;
-	}
-
-	void ProcessManager::SetData(ProcessContainer* data)
-	{
-		if (!sInstance || !data) { return; }
-
-		std::mutex mutex;
-		std::unique_lock<std::mutex> lock(mutex);
-		auto& lc = GetLockContainer();
-
-		while (mDataBusy)
-		{
-			if (!IsRunning()) { return; }
-			lc.Wait(lock);
-		}
-		lock.unlock();
-
-		mDataReady = false;
-		std::lock(mutex, lc.GetMutex());
-		{
-			std::lock_guard lock1(mutex, std::adopt_lock);
-			std::lock_guard lock2(lc.GetMutex(), std::adopt_lock);
-
-			mProcessContainer.reset(data);
-		}
-		mDataReady = true;
-		lc.NotifyAll();
-	}
-
-	bool ProcessManager::UpdateProcess(const ProcessEntry* entry) const
-	{
-		if (!entry) { return false; }
-
-		if (const auto& proc = mProcessMap.Find(entry->GetProcessId()))
-		{
-			std::mutex mutex;
-			std::lock(mutex, proc->Mutex());
-			{
-				std::lock_guard lock(mutex, std::adopt_lock);
-				std::lock_guard lock2(proc->Mutex(), std::adopt_lock);
-				if (proc != entry) { // Don't update if there's no change
-					proc->operator=(entry);
-				}
-
-				proc->Running() = true;
-			}
-
-			return true;
-		}
-		return false;
-	}
-
-	bool ProcessManager::UpdateProcess(const PROCESSENTRY32& pe32) const
-	{
-		if (const auto& proc = mProcessMap.Find(pe32.th32ProcessID))
-		{
-			std::mutex mutex;
-			std::lock(mutex, proc->Mutex());
-			{
-				std::lock_guard lock(mutex, std::adopt_lock);
-				std::lock_guard lock2(proc->Mutex(), std::adopt_lock);
-
-				if (proc->operator!=(pe32)) { // Don't update if there's no change
-					proc->operator=(pe32);
-				}
-
-				proc->Running() = true;
-			}
-
-			return true;
-		}
-		return false;
-	}
-
-	void ProcessManager::CleanMap()
-	{
-		std::mutex mutex;
-		std::lock(mutex, mProcessMap.GetMutex());
-		{
-			std::lock_guard lock(mutex, std::adopt_lock);
-			std::lock_guard lock2(mProcessMap.GetMutex(), std::adopt_lock);
-
-			// Remove any processes not currently running
-			for (auto it = mProcessMap.begin(); it != mProcessMap.end(); ++it)
-			{
-				if (!it->second->Running()) {
-					mProcessMap.Erase(it->second);
-					it = mProcessMap.begin(); // Reset iterator!
-				}
-			}
-		}
-	}
-
-	void ProcessManager::ResetAllRunningStatus()
-	{
-		std::mutex mutex;
-		std::lock(mutex, mProcessMap.GetMutex());
-		{
-			std::lock_guard lock(mutex, std::adopt_lock);
-			std::lock_guard lock2(mProcessMap.GetMutex(), std::adopt_lock);
-
-			// Set all process running status to false
-			for (const auto& [id, entry] : mProcessMap)
-			{
-				entry->Running() = false;
-			}
-		}
-	}
+ProcessManager* ProcessManager::sInstance = nullptr;
+
+ProcessManager::ProcessManager()
+    : ConcurrentProcess("ProcessManager")
+    , mUpdateInterval(TimeTick::Rate::Normal)
+    , mDataReady(false)
+    , mDataBusy(false)
+{
+    mProcessContainer.reset(new ProcessContainer);
+}
+
+ProcessManager::~ProcessManager()
+{
+    Time::Sleep(mUpdateInterval); // Let detached threads finish before
+
+    std::mutex mutex;
+    std::unique_lock<std::mutex> lock(mutex);
+
+    auto& lc = GetLockContainer();
+    while (mDataBusy) {
+        lc.Wait(lock);
+    }
+}
+
+void ProcessManager::Destroy() const
+{
+    sInstance = nullptr;
+    delete this;
+}
+
+ProcessManager* ProcessManager::Get()
+{
+    if (!sInstance) {
+        sInstance = new ProcessManager();
+    }
+
+    return sInstance;
+}
+
+std::shared_ptr<ProcessContainer> ProcessManager::GetData()
+{
+    RS_CORE_ASSERT(IsRunning(), "Process is not currently running! Call 'ProcessManager::Run()' to start process.");
+
+    if (!mDataReady) {
+        return {};
+    } // Is data is still not ready, return nothing.
+
+    mDataBusy = true;
+
+    return mProcessContainer;
+}
+
+void ProcessManager::ReleaseData()
+{
+    RS_CORE_ASSERT(mDataBusy, "Data is not owned! Did you forget to call 'GetData()'?");
+    mDataBusy = false;
+    auto& lc = GetLockContainer();
+    lc.NotifyAll();
+}
+
+void ProcessManager::SetUpdateInterval(Timestep interval)
+{
+    mUpdateInterval = interval;
+}
+
+uint32_t ProcessManager::GetUpdateSpeed() const
+{
+    return mUpdateInterval;
+}
+
+bool ProcessManager::IsRunning() const
+{
+    return mRunning;
+}
+
+int ProcessManager::GetNumProcesses() const
+{
+    return (mProcessContainer) ? mProcessContainer->GetNumEntries() : 0;
+}
+
+void ProcessManager::Run()
+{
+    if (!sInstance) {
+        sInstance = Get();
+    }
+
+    if (!sInstance->IsRunning()) {
+        sInstance->mRunning = true;
+
+        auto& app = Application::Get();
+        auto& threadPool = app.GetThreadPool();
+
+        threadPool.Queue([&] { sInstance->PrepareDataThread(); });
+        threadPool.Queue([&] { sInstance->ProcessDataThread(); });
+    }
+}
+
+void ProcessManager::Stop()
+{
+    if (sInstance && sInstance->IsRunning()) {
+        sInstance->mRunning = false;
+        auto& lc = sInstance->GetLockContainer();
+        lc.NotifyAll();
+    }
+}
+
+void ProcessManager::Shutdown()
+{
+    if (sInstance) {
+        Stop();
+
+        auto& app = Application::Get();
+        auto& threadPool = app.GetThreadPool();
+        threadPool.Queue([&]() { sInstance->Destroy(); });
+        sInstance = nullptr;
+    }
+}
+
+void ProcessManager::PrepareDataThread()
+{
+    auto& lc = GetLockContainer();
+
+    while (IsRunning()) {
+        if (PrepareData()) {
+            // Notify waiting threads
+            mDataPrepared = true;
+            lc.NotifyAll();
+        }
+        Time::Sleep(mUpdateInterval);
+    }
+}
+
+void ProcessManager::ProcessDataThread()
+{
+    while (IsRunning()) {
+        auto* data = GetPreparedData();
+        SetData(data);
+    }
+}
+
+bool ProcessManager::PrepareData()
+{
+    HANDLE hProcessSnap {};
+    PROCESSENTRY32 processEntry {};
+    processEntry.dwSize = sizeof(PROCESSENTRY32);
+
+    bool error = false;
+    bool snapshotReady = false;
+    bool processDone = false;
+    bool walkingDone = false;
+
+    auto& app = Application::Get();
+    auto& threadPool = app.GetThreadPool();
+
+    threadPool.Queue([&] {
+        snapshotReady = false;
+        // Take a snapshot of all processes in the system.
+        hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        if (hProcessSnap == INVALID_HANDLE_VALUE) {
+            PrintWin32Error("CreateToolhelp32Snapshot (of processes)");
+            error = true;
+        }
+        snapshotReady = true;
+    });
+
+    while (!snapshotReady) {
+        Time::Sleep(1);
+        if (error || !IsRunning()) {
+            return false;
+        }
+    }
+
+    threadPool.Queue([&] {
+        processDone = false;
+        if (!Process32First(hProcessSnap, &processEntry)) {
+            PrintWin32Error("Process32First");
+            CloseHandle(hProcessSnap); // clean the snapshot object
+            error = true;
+        }
+        processDone = true;
+    });
+
+    ResetAllRunningStatus();
+
+    // Now walk the snapshot of processes, and
+    // get information about each process in turn
+
+    while (!processDone) {
+        Time::Sleep(1);
+        if (error || !IsRunning()) {
+            return false;
+        }
+    }
+
+    threadPool.Queue([&, this] {
+        walkingDone = false;
+        do {
+            {
+                std::lock_guard lock2(mProcessMap.GetMutex());
+
+                // Set process running status to true and
+                //	update process, if applicable
+                if (!UpdateProcess(processEntry)) {
+                    // Otherwise, add new process
+                    mProcessMap.Emplace(new ProcessEntry(processEntry));
+                }
+            }
+
+            /* TODO: Implement these features
+            // List the modules and threads associated with this process
+            ListProcessModules(procEntry.th32ProcessID);
+            ListProcessThreads(procEntry.th32ProcessID);
+            */
+
+        } while (Process32Next(hProcessSnap, &processEntry));
+
+        walkingDone = true;
+
+        CloseHandle(hProcessSnap);
+    });
+
+    while (!walkingDone) {
+        Time::Sleep(1);
+        if (error || !IsRunning()) {
+            return false;
+        }
+    }
+
+    // Remove any processes not currently running
+    CleanMap();
+
+    return true;
+}
+
+ProcessContainer* ProcessManager::GetPreparedData()
+{
+    std::mutex mutex;
+    std::unique_lock<std::mutex> lock(mutex);
+
+    auto& lc = GetLockContainer();
+    while (!mDataPrepared) {
+        if (!IsRunning()) {
+            return nullptr;
+        }
+        lc.Wait(lock);
+    }
+
+    ProcessContainer* data = nullptr;
+    lock.unlock();
+    std::lock(mutex, mProcessMap.GetMutex());
+    {
+        std::lock_guard lock1(mutex, std::adopt_lock);
+        std::lock_guard lock2(mProcessMap.GetMutex(), std::adopt_lock);
+
+        // Add a deep copy to the ProcessContainer to preserve mProcessMap data
+        data = new ProcessContainer();
+        for (auto& [id, entry] : mProcessMap) {
+            auto* copy = new ProcessEntry(entry);
+            data->AddEntry(copy);
+        }
+    }
+    // Unset the flag
+    mDataPrepared = false;
+    lc.NotifyAll();
+
+    return data;
+}
+
+void ProcessManager::SetData(ProcessContainer* data)
+{
+    if (!sInstance || !data) {
+        return;
+    }
+
+    std::mutex mutex;
+    std::unique_lock<std::mutex> lock(mutex);
+    auto& lc = GetLockContainer();
+
+    while (mDataBusy) {
+        if (!IsRunning()) {
+            return;
+        }
+        lc.Wait(lock);
+    }
+    lock.unlock();
+
+    mDataReady = false;
+    std::lock(mutex, lc.GetMutex());
+    {
+        std::lock_guard lock1(mutex, std::adopt_lock);
+        std::lock_guard lock2(lc.GetMutex(), std::adopt_lock);
+
+        mProcessContainer.reset(data);
+    }
+    mDataReady = true;
+    lc.NotifyAll();
+}
+
+bool ProcessManager::UpdateProcess(const ProcessEntry* entry) const
+{
+    if (!entry) {
+        return false;
+    }
+
+    if (const auto& proc = mProcessMap.Find(entry->GetProcessId())) {
+        std::mutex mutex;
+        std::lock(mutex, proc->Mutex());
+        {
+            std::lock_guard lock(mutex, std::adopt_lock);
+            std::lock_guard lock2(proc->Mutex(), std::adopt_lock);
+            if (proc != entry) { // Don't update if there's no change
+                proc->operator=(entry);
+            }
+
+            proc->Running() = true;
+        }
+
+        return true;
+    }
+    return false;
+}
+
+bool ProcessManager::UpdateProcess(const PROCESSENTRY32& pe32) const
+{
+    if (const auto& proc = mProcessMap.Find(pe32.th32ProcessID)) {
+        std::mutex mutex;
+        std::lock(mutex, proc->Mutex());
+        {
+            std::lock_guard lock(mutex, std::adopt_lock);
+            std::lock_guard lock2(proc->Mutex(), std::adopt_lock);
+
+            if (proc->operator!=(pe32)) { // Don't update if there's no change
+                proc->operator=(pe32);
+            }
+
+            proc->Running() = true;
+        }
+
+        return true;
+    }
+    return false;
+}
+
+void ProcessManager::CleanMap()
+{
+    std::mutex mutex;
+    std::lock(mutex, mProcessMap.GetMutex());
+    {
+        std::lock_guard lock(mutex, std::adopt_lock);
+        std::lock_guard lock2(mProcessMap.GetMutex(), std::adopt_lock);
+
+        // Remove any processes not currently running
+        for (auto it = mProcessMap.begin(); it != mProcessMap.end(); ++it) {
+            if (!it->second->Running()) {
+                mProcessMap.Erase(it->second);
+                it = mProcessMap.begin(); // Reset iterator!
+            }
+        }
+    }
+}
+
+void ProcessManager::ResetAllRunningStatus()
+{
+    std::mutex mutex;
+    std::lock(mutex, mProcessMap.GetMutex());
+    {
+        std::lock_guard lock(mutex, std::adopt_lock);
+        std::lock_guard lock2(mProcessMap.GetMutex(), std::adopt_lock);
+
+        // Set all process running status to false
+        for (const auto& [id, entry] : mProcessMap) {
+            entry->Running() = false;
+        }
+    }
+}
 
 }
